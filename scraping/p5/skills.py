716dbd1c08c4f576f67c18add350713ba3cf36c8
AnashiKitsun <diamondguy9700@gmail.com>
2021-01-22 16:51:39 -0700

    Added new skill categories

--- scraping/p5/skills.py
+++ scraping/p5/skills.py
@@ -9,27 +9,44 @@ def parseSkills(skillList, targeting, type, mapping):
     # Finally, use mapping scheme to read in the rest of the values, if they exist
     for row in skillList[1:]:
         cols = row.select("td")
-        # Get name from col[0]
-        if cols[0].text.strip()[-1] != "★":
-            name = cols[0].text.strip()
-        else:
-            name = cols[0].text.strip()[:-1]
+
+        # Init skill vars read from name
+        royal = False #/ Denoted with †
+        equipped = False #/ Denoted with ⌃ #! ALSO ACTS AS †
+        dlc = False #/ Denoted with ↓
+
+        ## Get name from col[0], and read vars in.
+        name = cols[0].text.strip()        
+        # Check for †
+        royal = ("†" in name)
+        name = name.replace("†", "")
+        # Check for ⌃
+        equipped = ("⌃" in name)
+        if (equipped):
+            royal = True
+        name = name.replace("⌃", "")
+        # Check for ↓
+        dlc = ("↓" in name)
+        name = name.replace("↓", "")
+
         # Initialize skill data
         skills[name] = {}
-        # Detect DLC data
-        dlc = (cols[0].text.strip()[-1] == "★")
-        # Add type, targeting, and DLC data
+        # Add type, targeting, and name read data
         skills[name]["Targeting"] = targeting
         skills[name]["Type"] = type
-        skills[name]["DLC Exclusive"] = dlc
-        # Loop through mapping and add to skill
+        skills[name]["Royal"] = royal
+        skills[name]["Equip Only"] = equipped
+        skills[name]["DLC"] = dlc
+
+        ## Loop through cols and add to skill
+        # Make an easier var to loop through
+        colsNoName = cols[1:]
         for i, value in enumerate(mapping):
             if value != "None":
-                # So that mapping can skip with the value "None", and set any nonexistant values to "None"
-                if len(mapping)-1 <= i:
+                if i+1 > len(colsNoName):
                     skills[name][value] = "None"
                 else:
-                    skills[name][value] = cols[i+1].text.strip()
+                    skills[name][value] = colsNoName[i].text.strip()
 
 skills = {}
 skillSource = requests.get("https://megamitensei.fandom.com/wiki/List_of_Persona_5_Royal_Skills").content
@@ -38,80 +55,51 @@ skillSoup = BeautifulSoup(skillSource, features="lxml")
 map = ["Effect", "Cost", "Card"]
 altMap = ["Effect", "None", "None", "Cost", "Card"]
 
+tables = skillSoup.select(".table.p5")
+
 ###################
 # PHYSICAL SKILLS #
 ###################
 
 # Phys single target
-parseSkills(skillSoup.select_one("#tabber-440594ee4c55cc01260fd6895925888b > div:nth-child(2) > table:nth-child(2) > tbody:nth-child(1)").select("tr"), "e1", "Physical", map)
+parseSkills(tables[0].select("tr"), "e1", "Physical", map)
 
 # Phys multi target
-parseSkills(skillSoup.select_one("#tabber-440594ee4c55cc01260fd6895925888b > div:nth-child(3) > table:nth-child(2) > tbody:nth-child(1)").select("tr"), "e*", "Physical", map)
+parseSkills(tables[1].select("tr"), "e*", "Physical", map)
 
 # Gun general
-parseSkills(skillSoup.select("table.table")[1].select("tr"), "??", "Gun", map)
+parseSkills(tables[2].select("tr"), "e?", "Gun", map)
 
 ################
 # MAGIC SKILLS #
 ################
 
 # Fire General
-parseSkills(skillSoup.select_one("table.table:nth-child(15) > tbody:nth-child(1)").select("tr"), "??", "Fire", map)
+parseSkills(tables[3].select("tr"), "e?", "Fire", map)
 
-##################
-# PASSIVE SKILLS #
-##################
-pasSource = requests.get("https://samurai-gamers.com/persona-5/passive-skills-list/").content
-pasSoup = BeautifulSoup(pasSource, features="lxml")
-pasMap = ["Effect"]
+# Ice General
+parseSkills(tables[4].select("tr"), "e?", "Ice", map)
 
-for table in pasSoup.select("tbody")[:7]:
-    tableSkills = table.select("tr")
-    parseSkills(tableSkills, "Passive", "Passive", pasMap, 2)
+# Electric General
+parseSkills(tables[5].select("tr"), "e?", "Electric", map)
 
-##################
-# SUPPORT SKILLS #
-##################
-supSource = requests.get("https://samurai-gamers.com/persona-5/support-skills-list/").content
-supSoup = BeautifulSoup(supSource, features="lxml")
-supMap = ["Effect", "Cost"]
+# Wind General
+parseSkills(tables[6].select("tr"), "e?", "Wind", map)
 
-for table in supSoup.select("tbody")[:5]:
-    tableSkills = table.select("tr")
-    parseSkills(tableSkills, "Varied", "Support", supMap, 3)
+# Psy General
+parseSkills(tables[7].select("tr"), "e?", "Psy", map)
 
-##################
-# HEALING SKILLS #
-##################
-healSource = requests.get("https://samurai-gamers.com/persona-5/healing-skills-list/").content
-healSoup = BeautifulSoup(healSource, features="lxml")
-healMap = ["Effect", "Cost"]
+# Nuclear General
+parseSkills(tables[8].select("tr"), "e?", "Nuclear", map)
 
-for table in healSoup.select("tbody")[:2]:
-    tableSkills = table.select("tr")
-    parseSkills(tableSkills, "Varied", "Healing", healMap, 3)
+# Bless General
+parseSkills(tables[9].select("tr"), "e?", "Bless", altMap)
 
-###################
-# RECOVERY SKILLS #
-###################
-recSource = requests.get("https://samurai-gamers.com/persona-5/status-recovery-skills-list/").content
-recSoup = BeautifulSoup(recSource, features="lxml")
-recMap = ["Effect", "Cost"]
+# Curse General
+parseSkills(tables[10].select("tr"), "e?", "Curse", altMap)
 
-for table in recSoup.select("tbody")[:1]:
-    tableSkills = table.select("tr")
-    parseSkills(tableSkills, "SingleAll", "Healing", recMap, 3)
-
-##################
-# AILMENT SKILLS #
-##################
-ailSource = requests.get("https://samurai-gamers.com/persona-5/status-ailments-list/").content
-ailSoup = BeautifulSoup(ailSource, features="lxml")
-ailMap = ["Cost", "None", "Effect"]
-
-for table in ailSoup.select("tbody")[:3]:
-    tableSkills = table.select("tr")
-    parseSkills(tableSkills, "Varied", "Ailment", ailMap, 3)
+# Almighty general
+parseSkills(tables[11].select("tr"), "e?", "Almighty", altMap)
 
 ########
 # TEST #
